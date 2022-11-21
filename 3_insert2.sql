6097ecaf77821686b9d667bd08fdfcac2025f0c9
micserv010821 <dotnet290320@gmail.com>
2022-11-21 18:33:55 +0200

    upload all

---
+++ 3_insert2.sql
@@ -0,0 +1,6 @@
+INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (2, 'Allen', 25, 'Texas', 15000.00 ),
+VALUES (3, 'Teddy', 23, 'Norway', 20000.00 ),
+VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 ),
+VALUES (5, 'David', 27, 'Texas', 85000.00 ),
+VALUES (6, 'Kim', 22, 'South-Hall', 45000.00 );
